7917a30aa8715b0c16ef29263cc7e5761c050ccc
zhuwhr <zhuwhr@gmail.com>
2017-02-21 11:02:21 -0800

    set the api for reading and writing users

--- drf_demo1/drf_demo1/urls.py
+++ drf_demo1/drf_demo1/urls.py
@@ -15,7 +15,30 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from rest_framework import routers, serializers, viewsets
+from django.contrib.auth.models import User
+
+
+# Serializers to define the api representation
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'is_staff')
+
+
+# ViewSets define the view behavior
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+# automatically determining the URLconf. by routers
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
